a05e6d7589f6eb40b10958ea44ed1a04a4940434
Richard <z1ku1337@gmail.com>
2023-05-15 21:41:07 +0200

    controlando mala id por url

--- php/noticia.php
+++ php/noticia.php
@@ -16,7 +16,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ticket</title>
+    <title>Noticia</title>
     <link rel="stylesheet" href="../css/estilos.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -45,31 +45,26 @@
         }
     ?>
     <main>
-        <section id="ticket" class="seccion">
+        <section id="noticia" class="seccion">
             <?php
-                if(!isset($_GET['id_ticket']) || $tipo_usu!="admin"){
+                if(!isset($_GET['id_noticia']) || $tipo_usu!="admin"){
                     header("Location:../index.php");
                 }else{
-                    $id_ticket=$_GET['id_ticket'];
+                    $id_noticia=$_GET['id_noticia'];
 
-                    $datos_ticket=ticket_por_id($id_ticket);
+                    $datos_noticia=obtener_noticia_por_id($id_noticia);
 
-                    if($datos_ticket==null){
+                    if($datos_noticia==null){
                         header("Location:../index.php");
                     }else{
-                        $fecha_formateada=date("d-m-Y",strtotime($datos_ticket['fecha']));
+                        $fecha_formateada=date("d-m-Y",strtotime($datos_noticia['fecha']));
 
-                        echo '<div class="contenedorTicket">
-                            <p>ID ticket: '.$datos_ticket['id'].'</p>
-                            <p>Nick: '.$datos_ticket['nick'].'</p>
+                        echo '<div class="contenedorNoticia">
+                            <img src="../img/noticia/'.$datos_noticia['foto'].'">
+                            <h1>'.$datos_noticia['titulo'].'</h1>
+                            <p>'.$datos_noticia['texto'].'</p>
                             <p>Fecha: '.$fecha_formateada.'</p>
-                            <p>Asunto: '.$datos_ticket['asunto'].'</p>
-                            <p>Mensaje: '.$datos_ticket['texto'].'</p>
                         </div>';
-
-                        if($datos_ticket['foto']!=null){
-                            echo "<img src=\"../img/ticket/$datos_ticket[foto]\">";
-                        }
                     }
                 }
             ?>
